c666b855a360fc69f615bf8327f03414307908e7
ilizarov1 <1224722105@qq.com>
2021-10-30 20:49:21 +0800

    实现主页组件复用

--- pages/list.jsx
+++ pages/list.jsx
@@ -1,10 +1,5 @@
-import Head from 'next/head';
-import { Row, Col, Breadcrumb, List } from 'antd';
-import React, { useState } from 'react';
-import Header from '../components/Header/Header';
-import Avatar from '../components/Author';
-import Footer from '../components/footer';
-import styles from '../styles/index.module.css';
+import { Breadcrumb, List } from 'antd';
+import Main from '../components/Main';
 export default function myList() {
   const [mylist, setMylist] = useState([
     {
@@ -30,41 +25,35 @@ export default function myList() {
   ]);
   return (
     <>
-      <Head>
-        <title>List</title>
-      </Head>
-      <Header />
-      <Row className='comm-main' type='flex' justify='center'>
-        <Col className='comm-left' xs={24} sm={24} md={16} lg={18} xl={14}>
-          <Breadcrumb>
-            <Breadcrumb.Item>
-              <a href='/'>首页</a>
-            </Breadcrumb.Item>
-            <Breadcrumb.Item>列表</Breadcrumb.Item>
-          </Breadcrumb>
-          <List
-            header={<div>最新日志</div>}
-            itemLayout='vertical'
-            dataSource={mylist}
-            renderItem={(item) => (
-              <List.Item>
-                <div className={styles['list-title']}>{item.title}</div>
-                <div className={styles['list-icon']}>
-                  <span>2021 - 10 - 22</span>
-                  <span>随便写的</span>
-                  <span>有点菜</span>
-                </div>
-                <div className={styles['list-context']}>{item.context}</div>
-              </List.Item>
-            )}
-          />
-        </Col>
-
-        <Col className='comm-right' xs={0} sm={0} md={7} lg={5} xl={4}>
-          <Avatar />
-        </Col>
-      </Row>
-      <Footer />
+      <Main
+        title='List'
+        left={
+          <>
+            <Breadcrumb>
+              <Breadcrumb.Item>
+                <a href='/'>首页</a>
+              </Breadcrumb.Item>
+              <Breadcrumb.Item>列表</Breadcrumb.Item>
+            </Breadcrumb>
+            <List
+              header={<div>最新日志</div>}
+              itemLayout='vertical'
+              dataSource={mylist}
+              renderItem={(item) => (
+                <List.Item>
+                  <div className={styles['list-title']}>{item.title}</div>
+                  <div className={styles['list-icon']}>
+                    <span>2021 - 10 - 22</span>
+                    <span>随便写的</span>
+                    <span>有点菜</span>
+                  </div>
+                  <div className={styles['list-context']}>{item.context}</div>
+                </List.Item>
+              )}
+            />
+          </>
+        }
+      />
     </>
   );
 }
